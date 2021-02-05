60ec7d700c1482dc53d7c29d6f9cc2e31d4da73c
vagrant <galit.gerasimov3@gmail.com>
2021-02-05 00:52:59 +0000

    Updated the add property function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -211,7 +211,7 @@ const addProperty = function(property) {
   `;
   const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
   
-  return db.query(queryString, values)
+  return query(queryString, values)
     .then(res => {
       return res.rows[0];
     })
